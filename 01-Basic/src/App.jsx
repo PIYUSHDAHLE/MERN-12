36acb12dedebc65a7db2091817dfbd819e66cfe2
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-11-22 17:35:14 +0530

    Basic of react js

---
+++ 01-Basic/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[100vh] flex-col bg-purple-950 text-pink-400">
+      <h1>Welcome</h1>
+      <h2>To</h2>
+      <h1>React JS</h1>
+    </div>
+  );
+};
+
+export default App;
